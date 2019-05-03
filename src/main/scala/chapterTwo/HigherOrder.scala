f9c033794161862a51a8acc5f01b2596ed5b9a96
Will Farrell <farrell.will.g@gmail.com>
2019-05-03 11:30:37 -0500

    Add factorial.

--- src/main/scala/chapterTwo/HigherOrder.scala
+++ src/main/scala/chapterTwo/HigherOrder.scala
@@ -1,6 +1,25 @@
 package chapterTwo
 
 object HigherOrder {
+  def abs(n: Int): Int = {
+    if(n < 0) -n
+    else n
+  }
+
+  def formatHigher(x: Int, name: String,f: Int => Int): String = {
+   val msg = "The %s of %d is %d"
+    msg.format(x, name, f(x))
+  }
+
+  def factorial(n: Int): Int = {
+    @annotation.tailrec
+    def go(count: Int, acc: Int): Int = {
+      if (count <= 1) acc
+      else go(count - 1, acc * count)
+    }
+    go(n, 1)
+  }
+
   def fib(n: Int): Int = {
     if (n <= 1) 0
     else if (n == 2 || n == 3) 1
@@ -8,6 +27,7 @@ object HigherOrder {
   }
 
   def tailFib(n: Int): Int = {
+    @annotation.tailrec
     def go(count: Int, prev: Int, cur: Int): Int = {
       if (count >= n) cur
       else go(count + 1, cur, prev + cur)
--- src/test/scala/chapterTwo/HigherOrderSpec.scala
+++ src/test/scala/chapterTwo/HigherOrderSpec.scala
@@ -3,17 +3,25 @@ package chapterTwo
 import org.scalatest.FunSpec
 
 class HigherOrderSpec extends FunSpec{
-  describe("Higher Order functions"){
-    it("Finds nth fibonacci number"){
+  describe("Fibonacci"){
+    it("Finds nth number"){
       //The 15th number in the fibonacci sequence is 377
       val expected = 377
       val actual = HigherOrder.fib(15)
       assert(expected == actual)
     }
-    it("Finds nth fibonacci number using local tail-recursive function"){
+    it("Finds nth number using local tail-recursive function"){
       //The sixth number in the fibonacci sequence is 5
       val expected = 377
       val actual = HigherOrder.tailFib(15)
+      assert(expected == actual)
+    }
+  }
+  describe("Factorial"){
+    it("Finds the nth number"){
+      //The 4th number factorial is 24
+      val expected = 24
+      val actual = HigherOrder.factorial(4)
       assert(expected == actual)
     }
   }
