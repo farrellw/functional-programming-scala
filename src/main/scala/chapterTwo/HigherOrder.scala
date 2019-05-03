3e79d36cfbe7a0bc34d8fa5d591379ed3d89a781
Will Farrell <farrell.will.g@gmail.com>
2019-05-03 14:18:44 -0500

    Add functions and tests for isSorted, curry, uncurry, compose.

--- src/main/scala/chapterTwo/HigherOrder.scala
+++ src/main/scala/chapterTwo/HigherOrder.scala
@@ -2,13 +2,13 @@ package chapterTwo
 
 object HigherOrder {
   def abs(n: Int): Int = {
-    if(n < 0) -n
+    if (n < 0) -n
     else n
   }
 
-  def formatHigher(x: Int, name: String,f: Int => Int): String = {
-   val msg = "The %s of %d is %d"
-    msg.format(x, name, f(x))
+  def formatResult(x: Int, name: String, f: Int => Int): String = {
+    val msg = "The %s of %d is %d."
+    msg.format(name, x, f(x))
   }
 
   def factorial(n: Int): Int = {
@@ -17,6 +17,7 @@ object HigherOrder {
       if (count <= 1) acc
       else go(count - 1, acc * count)
     }
+
     go(n, 1)
   }
 
@@ -34,5 +35,30 @@ object HigherOrder {
     }
 
     go(2, 0, 1)
+  }
+
+  def isSorted[A](array: Array[A], f: (A, A) => Boolean): Boolean = {
+    def go(a: Array[A], pos1: Int, pos2: Int): Boolean = {
+      if (a.length <= pos2) true
+      else {
+        val sorted: Boolean = f(a(pos1), a(pos2))
+        if (sorted) go(a, pos2, pos2 + 1)
+        else return false
+      }
+    }
+
+    go(array, 0, 1)
+  }
+
+  def curry[A, B, C](f: (A, B) => C): A => B => C = {
+    a: A => (b: B) => f(a, b)
+  }
+
+  def uncurry[A,B,C](f: A => B => C): (A, B) => C = {
+    (a: A, b: B) => f(a)(b)
+  }
+
+  def compose[A,B,C](f: B => C, g: A => B): A => C = {
+    a: A => f(g(a))
   }
 }
--- src/test/scala/chapterTwo/HigherOrderSpec.scala
+++ src/test/scala/chapterTwo/HigherOrderSpec.scala
@@ -2,26 +2,99 @@ package chapterTwo
 
 import org.scalatest.FunSpec
 
-class HigherOrderSpec extends FunSpec{
-  describe("Fibonacci"){
-    it("Finds nth number"){
+class HigherOrderSpec extends FunSpec {
+  describe("Fibonacci") {
+    it("Finds nth number") {
       //The 15th number in the fibonacci sequence is 377
       val expected = 377
       val actual = HigherOrder.fib(15)
       assert(expected == actual)
     }
-    it("Finds nth number using local tail-recursive function"){
+    it("Finds nth number using local tail-recursive function") {
       //The sixth number in the fibonacci sequence is 5
       val expected = 377
       val actual = HigherOrder.tailFib(15)
       assert(expected == actual)
     }
   }
-  describe("Factorial"){
-    it("Finds the nth number"){
+  describe("Factorial") {
+    it("Finds the nth number") {
       //The 4th number factorial is 24
       val expected = 24
       val actual = HigherOrder.factorial(4)
+      assert(expected == actual)
+    }
+  }
+  describe("Format Result") {
+    it("Displays correct string") {
+      val expected = "The absolute value of -42 is 42."
+      val actual = HigherOrder.formatResult(-42, "absolute value", HigherOrder.abs)
+      assert(expected == actual)
+    }
+  }
+  describe("Is sorted") {
+    it("Returns false when an Array is not sorted") {
+      val expected = false
+      val unsortedList = Array(1, 3, 2)
+
+      def comparisonFunction(x: Int, y: Int): Boolean = {
+        x <= y
+      }
+
+      val actual = HigherOrder.isSorted(unsortedList, comparisonFunction)
+      assert(expected == actual)
+    }
+
+    it("Returns true when an Array is sorted") {
+      val expected = true
+      val unsortedList = Array(1, 2, 3)
+
+      def comparisonFunction(x: Int, y: Int): Boolean = {
+        x <= y
+      }
+
+      val actual = HigherOrder.isSorted(unsortedList, comparisonFunction)
+      assert(expected == actual)
+    }
+  }
+  describe("Curry") {
+    def f(x: String, y: Int): (String, Int) = (x, y)
+
+    it("Curried function executes the same as original function") {
+      val curriedFunction = HigherOrder.curry(f)
+      val expected = f("a", 1)
+      val actual = curriedFunction("a")(1)
+      assert(expected == actual)
+    }
+  }
+  describe("Uncurry") {
+    def f(x: String): Int => (String, Int) = {
+      y: Int => (x, y)
+    }
+
+    it("Uncurried function executes the same as original function") {
+      val uncurriedFunction = HigherOrder.uncurry(f)
+      val expected = f("a")(1)
+      val actual = uncurriedFunction("a", 1)
+      assert(expected == actual)
+    }
+  }
+
+  describe("Compose"){
+    def f(x: String): String = x.capitalize
+    def g(x: String): String = x.toLowerCase
+    val originalString: String = "sCAla"
+
+    it("Composed function executes the two functions given"){
+      val composed: String => String = HigherOrder.compose(f, g)
+      val expected: String = "Scala"
+      val actual = composed(originalString)
+      assert(expected == actual)
+    }
+    it("Composed function returns the same result as executing each function individually"){
+      val composed: String => String = HigherOrder.compose(f, g)
+      val expected = f(g(originalString))
+      val actual = composed(originalString)
       assert(expected == actual)
     }
   }
